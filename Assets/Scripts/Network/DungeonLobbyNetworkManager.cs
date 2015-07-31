c0fabb3a891d9d3bf9f9253d69b79f4428a4a7a5
Paximillian <morzil@walla.com>
2015-07-31 04:31:23 +0300

    More work on lobby manager

---
+++ Assets/Scripts/Network/DungeonLobbyNetworkManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class DungeonLobbyNetworkManager : NetworkLobbyManager {
+
+    [SerializeField]
+    private GameObject clientPrefab;
+    [SerializeField]
+    private GameObject serverPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override GameObject OnLobbyServerCreateLobbyPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        Debug.Log("Create lobby player");
+        return base.OnLobbyServerCreateLobbyPlayer(conn, playerControllerId);
+    }
+
+    public override GameObject OnLobbyServerCreateGamePlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject player = null;
+        Debug.Log(numPlayers);
+        if (numPlayers == 0)
+        {
+            Debug.Log("Server");
+            player = Instantiate(serverPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        }
+        else
+        {
+            Debug.Log("Client");
+            player = Instantiate(clientPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        }
+
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+        return player;
+    }
+}
