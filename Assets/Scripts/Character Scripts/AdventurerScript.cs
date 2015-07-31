87536f1689b2e6c971825a8589c0ba7a60537c35
dimkatz <dimkatz89@gmail.com>
2015-07-31 20:46:23 +0300

    Adventurer Stun mode

--- Assets/Scripts/Character Scripts/AdventurerScript.cs
+++ Assets/Scripts/Character Scripts/AdventurerScript.cs
@@ -2,11 +2,18 @@
 using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class AdventurerScript : PlayerScript
 {
     [SerializeField]
     private float m_MoveSpeed = 10;
+
+    [SyncVar]
+    private bool m_IsStunned = false;
+
+    private DateTime m_StunStartTime;
+    private int m_StunDuration;
 
 	// Use this for initialization
 	void Start () {
@@ -15,20 +22,43 @@ public class AdventurerScript : PlayerScript
 	
 	// Update is called once per frame
 	void Update () {
+        if (isServer)
+        {
+            handleStun();
+        }
+
         if (isLocalPlayer)
         {
             if (ControlsManager.TapActionRequested())
             {
                 Vector3 moveTo = ControlsManager.GetTapActionPoint();
-                CmdMovementManagement(moveTo);
+                if (!m_IsStunned)
+                {
+                    CmdMovementManagement(moveTo);
+                }
             }
         }
 
 	    if(Input.GetKeyDown(KeyCode.Space) == true)
         {
-            CmdMovementManagement(new Vector3(0.19f, -1.64f, 0f));
+            if (!m_IsStunned)
+            {
+                CmdMovementManagement(new Vector3(0.19f, -1.64f, 0f));
+            }
         }
 	}
+
+    private void handleStun()
+    {
+        if (m_IsStunned)
+        {
+            TimeSpan TimeInStun = DateTime.Now - m_StunStartTime;
+            if ((int)TimeInStun.TotalMilliseconds >= m_StunDuration)
+            {
+                m_IsStunned = false;
+            }
+        }
+    }
 
     [Command]
     private void CmdMovementManagement(Vector3 i_MoveTo)
@@ -37,6 +67,14 @@ public class AdventurerScript : PlayerScript
         StopCoroutine("MovePlayerAlongRoute");
         StartCoroutine(MovePlayerAlongRoute(roomPathToTarget));
     }
+
+    [Command]
+    public void CmdStunAdventurer(int i_StunDuration)
+    {
+        m_IsStunned = true;
+        m_StunDuration = i_StunDuration;
+        m_StunStartTime = DateTime.Now;
+     }
 
     [Server]
     private IEnumerator MovePlayerAlongRoute(Stack<Vector3> i_RoomPath)
