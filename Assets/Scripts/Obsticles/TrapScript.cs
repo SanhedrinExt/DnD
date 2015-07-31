0eed8b794f21d31fb79efb647b0cf8c5485e827a
dimkatz <dimkatz89@gmail.com>
2015-08-01 01:22:24 +0300

    Obsticle prafabs

---
+++ Assets/Scripts/Obsticles/TrapScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapScript : MonoBehaviour {
+
+    [SerializeField]
+    private const int k_TimeToStun = 4000;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter2D(Collider2D i_CollisionInfo)
+    {
+        AdventurerScript AdventurerInTrap =  i_CollisionInfo.gameObject.GetComponent<AdventurerScript>();
+        AdventurerInTrap.CmdStunAdventurer(k_TimeToStun);
+    }
+}
