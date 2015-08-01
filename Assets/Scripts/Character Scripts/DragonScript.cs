834274ed4b7d18992fcc06021915e8f421201a4d
eitanacton <eitanacton@gmail.com>
2015-08-01 09:39:08 +0300

    a

--- Assets/Scripts/Character Scripts/DragonScript.cs
+++ Assets/Scripts/Character Scripts/DragonScript.cs
@@ -82,17 +82,10 @@ public class DragonScript : PlayerScript
         }
         Vector2 v3 = new Vector2(room.transform.position.x, room.transform.position.y);
         Vector2 s2 = new Vector2(room.transform.localScale.x, room.transform.localScale.y);
-        RaycastHit2D[] hits = Physics2D.BoxCastAll(v3, s2, 0, Vector2.up, s2.x / 2, 1 << LayerMask.NameToLayer("Player"));
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(v3, s2, 0, Vector2.up, s2.x / 2, 1 << LayerMask.NameToLayer("Player") | 1 << LayerMask.NameToLayer("Dragon"));
 
-        foreach (RaycastHit2D hit in hits)
-        {
-            PlayerScript player = hit.transform.GetComponent<PlayerScript>();
-            if (player)
-            {
-                isInRoom = true;
-                break;
-            }
-        }
+        if (hits.Length == 0)
+            isInRoom = true;
 
         return isInRoom;
     }
