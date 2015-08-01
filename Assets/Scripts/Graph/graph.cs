1d34faa9ddefc15f5b9ce5f19a2e2000be0e2f7d
eitanacton <eitanacton@gmail.com>
2015-08-01 14:46:57 +0300

    g

--- Assets/Scripts/Graph/graph.cs
+++ Assets/Scripts/Graph/graph.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 
 public class Graph {
 
-    public const int c_y = 3;
-    public const int c_x = 3;
+    public const int c_y = 5;
+    public const int c_x = 5;
 
     private Graph() 
     {
