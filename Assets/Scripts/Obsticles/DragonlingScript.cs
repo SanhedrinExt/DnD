fa8cb568b37586718299933b83abff14adc7c550
dimkatz <dimkatz89@gmail.com>
2015-08-01 03:23:44 +0300

    dragonling

--- Assets/Scripts/Obsticles/DragonlingScript.cs
+++ Assets/Scripts/Obsticles/DragonlingScript.cs
@@ -39,5 +39,7 @@ public class DragonlingScript : NetworkBehaviour
 
     private void fire()
     {
+        GameObject fireBall = Instantiate(m_FireBallPrefab, transform.position, transform.rotation) as GameObject;
+        //fireBall.GetComponent<Rigidbody2D>().velocity = 
     }
 }
